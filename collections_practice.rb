d327e7a8e2fd5dd49018d5d631754b4546ce6569
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-18 20:10:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
-# your code goes here
+def begins_with_r(array)
+  button = true
+  answer = []
+  array.each {|element|
+    if element[0] != "r"
+      button = false
+    end
+  }
+  return button
+end
+
+def contain_a(array)
+  answer = []
+  array.each {|element|
+    answer.push(element) if element.split("").include?.include?("a")
+  }
+  return answer
+end
